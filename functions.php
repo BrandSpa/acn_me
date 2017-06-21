c7bb72834d8897d6efdc2282846190d570e2210d
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-21 11:41:56 -0500

    fix names

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 //SHORTCODES
 require_once 'shortcodes/fullpage.php';
-require_once 'shortcodes/fullpage_slides.php';
+require_once 'shortcodes/fullpage_slide.php';
 //VISUALCOMPOSER
 require_once 'shortcodes/vc/fullpage.php';
-require_once 'shortcodes/vc/fullpage_slides.php';
+require_once 'shortcodes/vc/fullpage_slide.php';
