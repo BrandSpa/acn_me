0bc7f194f56fba3c0b2ef9740d04033e31cf66c0
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-27 10:47:56 -0500

    remove script

--- footer.php
+++ footer.php
@@ -1,13 +1,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.4/vendors/scrolloverflow.js"></script>
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.4/jquery.fullpage.js"></script>
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/3.0.0/lazysizes.min.js"></script>
 <script src="https://afarkas.github.io/lazysizes/plugins/bgset/ls.bgset.min.js"></script>
-<script>
 
-</script>
 </body>
 </html>
